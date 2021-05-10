dca3d53daf193eee06288a3ea3ad0279dac2dc49
Klaaßen, Sven (BAM5698) <sven.klaassen@uni-hamburg.de>
2021-05-10 09:21:24 +0200

    add test for the robust methdo

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,5 @@
 library(testthat)
+library(patrick)
 library(GGMtest)
 
 test_check("GGMtest")
--- tests/testthat/test-GGMtest2.R
+++ tests/testthat/test-GGMtest2.R
@@ -14,6 +14,7 @@ test_that("Compare GGMtest and GGMtest2", {
 
   model2 <- GGMtest2(data = X,
                      edges = edges,
+                     method = "partialling out",
                      nuisance_estimaton = "sqrt-lasso")
 
   CR <- create_CR(model = model2,
@@ -49,3 +50,90 @@ test_that("Test volumes", {
   expect_equal(CR$vol_max,vol_max)
   expect_equal(CR$vol_sphere,vol_sphere)
   })
+
+test_cases <- expand.grid(
+  method = c("robust", "partialling out"),
+  DML_method = c("DML1", "DML2"),
+  nuisance_estimation = c("lasso", "post-lasso", "sqrt-lasso"),
+  stringsAsFactors = FALSE
+)
+
+test_cases["test_name"] = apply(test_cases, 1, paste, collapse = "_")
+
+patrick::with_parameters_test_that("Test different methods work similar over k-fold",
+  .cases = test_cases,
+  {
+    set.seed(42)
+    n <- 2000; p <- 10
+    X <- MASS::mvrnorm(n=n, mu=rep(0,p), Sigma=diag(p))
+
+    edges <- matrix(c(rep(1,p-1),2:p),ncol = 2)
+    model1 <- GGMtest2(data = X,
+                       edges = edges,
+                       method = method,
+                       nuisance_estimaton = nuisance_estimation,
+                       DML_method = DML_method,
+                       k_fold = 1)
+    model2 <- GGMtest2(data = X,
+                       edges = edges,
+                       method = method,
+                       nuisance_estimaton = nuisance_estimation,
+                       DML_method = DML_method,
+                       k_fold = 5)
+    expect_equal(model1$estimates, model2$estimates,tolerance = 1e-1)
+  }
+)
+
+test_cases <- expand.grid(
+  method = c("robust", "partialling out"),
+  DML_method = c("DML1", "DML2"),
+  nuisance_estimation = c("lasso", "post-lasso", "sqrt-lasso"),
+  k_fold = c(1,5),
+  stringsAsFactors = FALSE
+)
+
+test_cases["test_name"] = apply(test_cases, 1, paste, collapse = "_")
+
+patrick::with_parameters_test_that("Hypothesis testing",
+  .cases = test_cases,
+  {
+    set.seed(42)
+    n <- 500; p <- 10
+    X <- MASS::mvrnorm(n=n, mu=rep(0,p), Sigma=diag(p))
+
+    edges <- matrix(c(rep(1,p-1),2:p),ncol = 2)
+    model <- GGMtest2(data = X,
+                      edges = edges,
+                      method = method,
+                      nuisance_estimaton = nuisance_estimation,
+                      DML_method = DML_method,
+                      k_fold = k_fold)
+
+    CR1 <- create_CR(model = model,
+                     alpha = 0.05,
+                     B = 500,
+                     s = 1,
+                     exp = 1)
+    CR2 <- create_CR(model = model,
+                     alpha = 0.05,
+                     B = 500,
+                     s = 5,
+                     exp = 1)
+    CR3 <- create_CR(model = model,
+                     alpha = 0.05,
+                     B = 500,
+                     s = 5,
+                     exp = 2)
+    CR4 <- create_CR(model = model,
+                     alpha = 0.05,
+                     B = 500,
+                     s = 10,
+                     exp = 2)
+
+    expect_true(CR1$hyp_max)
+    expect_true(CR2$hyp_max)
+    expect_true(CR3$hyp_max)
+    expect_true(CR4$hyp_max)
+    }
+)
+
